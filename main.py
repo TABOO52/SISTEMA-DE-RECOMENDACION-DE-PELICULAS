c616d33e35894d84e4e532513dd030634716ce33
TABOO_LOCAL <fate8853@gmail.com>
2024-07-03 10:43:23 -0500

    Hello world API

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+# Crear una instancia de la aplicación FastAPI
+app = FastAPI()
+
+# Definir una ruta y una función para manejarla
+@app.get('/')
+def read_root():
+    return {"message": "Hello, World!"}
